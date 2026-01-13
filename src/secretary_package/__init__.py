8edce17f69583147d74dd2db30007688783e6b0f
olesia raduchych <lesyaraduchyc@gmail.com>
2026-01-13 20:41:06 +0200

    updated secretary_ package

--- src/secretary_package/__init__.py
+++ src/secretary_package/__init__.py
@@ -1,4 +1,20 @@
-#from .agent import agent_learner
 from .secretary import CooperativeSecretaryEnv
-#from .learn import *
-#from .utilfunctions import scale_state, single_shape_adaptor
+from .agent import agent_learner
+from .utilfunctions import (
+    scale_state, 
+    single_shape_adaptor, 
+    one_hot,
+    initializer,
+    update_state_step
+)
+
+__all__ = [
+    "CooperativeSecretaryEnv",
+    "agent_learner",
+    "scale_state",
+    "single_shape_adaptor",
+    "one_hot",
+    "initializer",
+    "update_state_step"
+]
+__version__ = "0.1.0"
